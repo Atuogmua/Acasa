8197492da2b4a2d3e29bf93deab90c1fa9c851f9
Toma Augustina <52568962+Atuogmua@users.noreply.github.com>
2024-04-10 19:36:12 +0300

    Update task_2.py

--- lectia 4/task_2.py
+++ lectia 4/task_2.py
@@ -3,103 +3,104 @@
 # Creează o variabilă numită `name` și seteaz-o la numele tău
 
 # CODUL TĂU VINE MAI JOS:
-
+name = "Toma Augustina"
 # CODUL TĂU VINE MAI SUS:
 
 # Acum afișează valoarea variabilei `name` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name)
 # CODUL TĂU VINE MAI SUS:
 
 # Acum creează o nouă variabilă numită `name2` și seteaz-o la valoarea variabilei `name` și afișează valoarea variabilei `name2` folosind funcția `print`
 
 # CODUL TĂU VINE MAI JOS:
-
+name2 = name
+print(name2)
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează ultimul caracter al variabilei `name` folosind indexarea
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name[len(name) - 1])
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează primele 3 caractere ale variabilei `name` folosind slicing
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name2[:3])
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează valoarea variabilei `name` în majuscule folosind metoda `upper`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name.upper())
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează valoarea variabilei `name` în minuscule folosind metoda `lower`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name.lower())
 # CODUL TĂU VINE MAI SUS:
 
 # Acum printează concatenarea variabilelor `name` și `name2`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(name + name2)
 # CODUL TĂU VINE MAI SUS:
 
 # Creează o variabilă `text` și setează-i un text la alegere, cu restricția ca acesta să conțină mai multe rânduri
 
 # CODUL TĂU VINE MAI JOS:
-
+text = "Wiki (pronunțat în engleză /ˈwɪki/) este o aplicație web ce permite utilizatorilor să adauge conținut și să păstreze propriile lor versiuni succesive, la fel ca pe un forum Internet, dar permite și oricui altcuiva să modifice conținutul. Wiki-urile fac parte din fenomenul recent numit Web 2.0. Termenul Wiki denumește și softwareul colaborativ folosit pentru crearea unui asemenea site web (vezi software Wiki)."
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă variabila `text` conține cuvântul `python`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(text.find("python"))
 # CODUL TĂU VINE MAI SUS:
 
 # Folosește metoda replace pentru a înlocui litera `a` din variabila `text` cu litera `e`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(text.replace("a", "e"))
 # CODUL TĂU VINE MAI SUS:
 
 # Folosește metoda split pentru a transforma variabila `text` într-o listă de cuvinte
 
 # CODUL TĂU VINE MAI JOS:
-
+print(text.split())
 # CODUL TĂU VINE MAI SUS:
 
 # Creează un f-string care să conțină variabilele `name` și `name2`
 
 # CODUL TĂU VINE MAI JOS:
-
+f = name + name2
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă string-ul creat se termină cu `!`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f.rfind("!") == len(f) - 1)
 # CODUL TĂU VINE MAI SUS:
 
 # Verifică dacă string-ul creat începe cu `Hello`
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f.rfind("Hello") == 0)
 # CODUL TĂU VINE MAI SUS:
 
 # Identifică indexul unde se află `!` în string-ul creat
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f.rfind("!"))
 # CODUL TĂU VINE MAI SUS:
 
 # Identifică indexul unde se află `o` în string-ul creat folosind altă metodă
 
 # CODUL TĂU VINE MAI JOS:
-
+print(f.find("o").__index__())
 # CODUL TĂU VINE MAI SUS:
 
 # Utilizând format string-uri, creează un string care să conțină variabilele `name` și `name2`
